433782a5f3327a1a44c616435e48e5152d87cdd5
Jtolen543 <Jtolen543@gmail.com>
2025-03-23 15:47:38 -0400

    Dashboard

--- frontend/src/app/dashboard/Dashboard.tsx
+++ frontend/src/app/dashboard/Dashboard.tsx
@@ -1,7 +1,6 @@
 import { AppSidebar } from "@/components/app-sidebar"
 import { SiteHeader } from "@/components/site-header"
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
-import React from "react"
 export default function Page() {
   return (
     <div className="[--header-height:calc(theme(spacing.14))]">
